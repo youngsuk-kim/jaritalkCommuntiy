3f192de3e873340aedf0cd752d1d5bfe44a1a8d4
bread <thayer2392@gmail.com>
2023-01-28 01:58:06 +0900

    chore: 구글 자바 컨벤션 적용, 불필요 클래스 제거

--- src/main/java/com/callbus/jaritalkcommunity/JaritalkCommunityApplication.java
+++ src/main/java/com/callbus/jaritalkcommunity/JaritalkCommunityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JaritalkCommunityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JaritalkCommunityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JaritalkCommunityApplication.class, args);
+    }
 
 }
