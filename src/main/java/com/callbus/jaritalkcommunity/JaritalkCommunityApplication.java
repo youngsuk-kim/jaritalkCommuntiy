0510f1309a6acb433729b6c28131d521e4e2d2fd
bread <thayer2392@gmail.com>
2023-01-28 01:51:39 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/callbus/jaritalkcommunity/JaritalkCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.callbus.jaritalkcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaritalkCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaritalkCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callbus/jaritalkcommunity/JaritalkCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callbus.jaritalkcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaritalkCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
